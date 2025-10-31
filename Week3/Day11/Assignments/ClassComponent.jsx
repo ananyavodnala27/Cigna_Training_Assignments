66463f06c60e0e78ea4b7d925969ef464e93f335
Ananya Vodnala <152397860+ananyavodnala27@users.noreply.github.com>
2025-11-01 01:16:34 +0530

    Create ClassComponent.jsx

---
+++ Week3/Day11/Assignments/ClassComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ClassComp extends React.Component{
+    render(){
+        return(
+            <h1>Hello World.</h1>
+        );
+    }
+}
+
+export default ClassComp;
