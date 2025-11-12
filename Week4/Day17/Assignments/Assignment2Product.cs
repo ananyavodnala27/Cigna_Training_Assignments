43faef4ffa826b80043e7f631832683851d9ff63
Ananya Vodnala <152397860+ananyavodnala27@users.noreply.github.com>
2025-11-12 22:40:09 +0530

    Updated Assignment2Product.cs

--- Week4/Day17/Assignments/Assignment2Product.cs
+++ Week4/Day17/Assignments/Assignment2Product.cs
@@ -6,10 +6,10 @@ namespace Assignment
 {
     public class Product
     {
-        public int productId { get; set; }
-        public string productName { get; set; }
-        public int unitPrice { get; set; }
-        public int quantity { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
 
     }
     internal class Program
@@ -19,16 +19,16 @@ namespace Assignment
 
             Product obj = new Product();
             Console.Write("Enter Product Id : ");
-            obj.productId = Convert.ToInt32(Console.ReadLine());
+            obj.ProductId = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter Product name : ");
-            obj.productName = Console.ReadLine();
+            obj.ProductName = Console.ReadLine();
             Console.Write("Enter Product's Unit Price : ");
-            obj.unitPrice = Convert.ToInt32(Console.ReadLine());
+            obj.UnitPrice = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the quantity : ");
-            obj.quantity = Convert.ToInt32(Console.ReadLine());
+            obj.Quantity = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Product Details");
-            Console.WriteLine($"Product Id : {obj.productId}, Product Name : {obj.productName}, Unit Price : {obj.unitPrice}, Quantity : {obj.quantity}");
+            Console.WriteLine($"Product Id : {obj.ProductId}, Product Name : {obj.ProductName}, Unit Price : {obj.UnitPrice}, Quantity : {obj.Quantity}");
 
         }
     }
