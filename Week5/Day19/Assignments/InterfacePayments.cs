96bc6a6e1dd06878e010255596e203ec94b24f09
Ananya Vodnala <152397860+ananyavodnala27@users.noreply.github.com>
2025-11-15 14:39:29 +0530

    Updated InterfacePayments.cs

--- Week5/Day19/Assignments/InterfacePayments.cs
+++ Week5/Day19/Assignments/InterfacePayments.cs
@@ -3,71 +3,92 @@ using System;
 
 namespace Assignment
 {
-    
     interface IPayment
     {
-        void pay(double amount); 
+        void Pay(double amount);
     }
-
-    class CreditCardPayment : IPayment
+    
+    public class CreditCardPayment : IPayment
     {
-        public string cardNumber { get; set; }
-        public string cardHolder { get; set; }
-
-        public CreditCardPayment(string cardNumber, string cardHolder)
-        {
-            this.cardNumber = cardNumber;
-            this.cardHolder = cardHolder;
-        }
-        public void pay(double amt)
-        {
-            Console.WriteLine($"Payment Details -\nPayment Method : Credit Card\nCard Number : {cardNumber}\nCard Holder : {cardHolder}\nAmount : {amt}");
-            Console.WriteLine("-----------------------------------------------------");
-        }
-    }
-
-    class UPIPayment : IPayment
-    {
-        public string paymentID { get; set; }
-
-        public UPIPayment(string paymentID)
-        {
-            this.paymentID = paymentID;
-        }
-        public void pay(double amt)
-        {
-            Console.WriteLine($"Payment Details -\nPayment Method : UPI\nPayment ID : {paymentID}\nAmount : {amt}");
-            Console.WriteLine("-----------------------------------------------------");
-        }
-    }
-
-    class PaypalPayment : IPayment
-    {
-        public string EmailID { get; set; }
-
-        public PaypalPayment(string EmailID)
-        {
-            this.EmailID = EmailID;
-        }
-        public void pay(double amt)
-        {
-            Console.WriteLine($"Payment Details -\nPayment Method : PayPal\nEmail ID : {EmailID}\nAmount : {amt}");
-            Console.WriteLine("-----------------------------------------------------");
-        }
-    }
-    internal class Program
-    {
+        public long CardNumber {get; set;}
+        public string CardHolderName {get; set;}
+        public DateTime ExpiryDate {get; set;}
+        public double Amount {get; set;}
         
-        static void Main(string[] args)
+        public CreditCardPayment(long cardNumber, string cardHolderName, DateTime expiryDate)
         {
-            IPayment CreditCard1 = new CreditCardPayment("123-456-789", "Ananya");
-            CreditCard1.pay(10000);
-
-            IPayment UPIPayment1 = new UPIPayment("abc89");
-            UPIPayment1.pay(5000);
-
-            IPayment PaypalPayment1 = new PaypalPayment("kate12@gmail.com");
-            PaypalPayment1.pay(8000);
+            CardNumber = cardNumber;
+            CardHolderName = cardHolderName;
+            ExpiryDate = expiryDate;
+        }
+        
+        public void Pay(double amount)
+        {
+            Amount = amount;
+            Console.WriteLine($"Credit Card Payment\nCard Number : {CardNumber}\nCard Holder Name : {CardHolderName}\nExpiry Date : {ExpiryDate.ToString("dd-MM-yyyy")}\nAmount : {Amount}");
+            Console.WriteLine("------------------------");
+        }
+    }
+    
+    public class UPIPayment : IPayment
+    {
+        public string UPI_ID {get; set;}
+        public string BankName {get; set;}
+        public double Amount {get; set;}
+        
+        public UPIPayment(string upiId, string bankName)
+        {
+            UPI_ID = upiId;
+            BankName = bankName;
+        }
+        
+        public void Pay(double amount)
+        {
+            Amount = amount;
+            Console.WriteLine($"UPI ID Payment\nUPI ID : {UPI_ID}\nBank Name : {BankName}\nAmount : {Amount}");
+            Console.WriteLine("------------------------");
+        }
+    }
+    
+    public class PaypalPayment : IPayment
+    {
+        public string EmailId {get; set;}
+        public double Amount {get; set;}
+        
+        public PaypalPayment(string emailId)
+        {
+            EmailId = emailId;
+        }
+        
+        public void Pay(double amount)
+        {
+            Amount = amount;
+            Console.WriteLine($"PayPal Payment\nEmail ID : {EmailId}\nAmount : {Amount}");
+            Console.WriteLine("------------------------");
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            IPayment creditCard1 = new CreditCardPayment(10632700, "Ananya", new DateTime(2025, 10, 31));
+            creditCard1.Pay(10000);
+            
+            IPayment creditCard2 = new CreditCardPayment(17282300, "Kavya", new DateTime(2025, 09, 16));
+            creditCard2.Pay(5000);
+            
+            IPayment UPIPayment1 = new UPIPayment("Kajal@upi", "SBI");
+            UPIPayment1.Pay(25000);
+            
+            IPayment UPIPayment2 = new UPIPayment("Gautham@upi", "HDFC");
+            UPIPayment2.Pay(18000);
+            
+            IPayment Paypal1 = new PaypalPayment("yamuna@gmail.com");
+            Paypal1.Pay(20000);
+            
+            IPayment Paypal2 = new PaypalPayment("abhinaya@gmail.com");
+            Paypal2.Pay(16000);
         }
     }
 }
